da6f2b7fe6bc4527ab19a2800135dc2f99161cf6
Tamfoolery <tamfoolery@live.co.uk>
2015-02-19 15:38:35 +0000

    Getting followers done, settings & view todo

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -4,3 +4,79 @@ ncg_f_cssEditor.getSession().setMode("ace/mode/css");
 ncg_f_cssEditor.setOptions({
 	maxLines: Infinity
 });
+
+/**
+ * Followers
+ */
+var ncg_f_channel = 'imaqtpie',
+	ncg_f_timer = false,
+	ncg_f_followPanelList = $('#ncg-f_followList'),
+	ncg_f_followers = {},
+	ncg_f_followersCount = -5;
+
+function ncg_f_getFollowers(all, offset) {
+	all = all || false;
+	offset = offset || 0;
+
+	if (offset > 1000) return;
+
+	if (all) {
+		$.getJSON('https://api.twitch.tv/kraken/channels/' + ncg_f_channel + '/follows?direction=desc&limit=5&offset=' + offset + '&callback=?', function (data) {
+			if (data.follows && data.follows.length > 0) {
+				data.follows.forEach(function (follower) {
+					ncg_f_followers[follower.user.name] = true;
+				});
+
+				ncg_f_getFollowers(true, offset + 5);
+			}
+		}).fail(function () {
+			setTimeout(function () {
+				ncg_f_getFollowers(true, offset);
+			}, 5000);
+		});
+	} else {
+		$.getJSON('https://api.twitch.tv/kraken/channels/' + ncg_f_channel + '/follows?direction=desc&limit=5&offset=' + offset + '&callback=?', function (data) {
+			if (data.follows) {
+				if (data['_total'] > 0 && ncg_f_followers.length === 0) {
+					data.follows.forEach(function (follower) {
+						ncg_f_followers[follower.user.name] = true;
+					});
+				} else {
+					data.follows.forEach(function (follower) {
+						if (!ncg_f_followers[follower.user.name]) {
+							ncg_f_followers[follower.user.name] = true;
+							ncg_f_newFollower(follower.user.display_name);
+						}
+					})
+				}
+			}
+		});
+	}
+}
+
+function ncg_f_newFollower(user) {
+	if (ncg_f_timer) {
+		setTimeout(function () {
+			newFollower(user);
+		}, 1000);
+
+		return;
+	}
+
+	ncg_f_followersCount++;
+	$('#ncg-f_totalNewFollowers').html(ncg_f_followersCount);
+
+	ncg_f_followPanelList.prepend('<li><span>' + user + '</span></li>');
+
+	if ($('#ncg-f_followList li').length > 5) {
+		ncg_f_followPanelList.find('li:last-child').addClass('ncg-f_remove');
+		setTimeout(function () {
+			ncg_f_followPanelList.find('li:last-child').remove();
+		}, 500);
+	}
+}
+
+$(document).ready(function () {
+	ncg_f_getFollowers();
+	setInterval(ncg_f_getFollowers, 1000);
+});
